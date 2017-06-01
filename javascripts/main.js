b6fc07a8b0703a70cf51b64af77d7cb04a04f1b6
Gilles Crettenand <gilles.crettenand@liip.ch>
2017-06-01 16:53:11 +0200

    first blood

---
+++ javascripts/main.js
@@ -0,0 +1,49 @@
+const grid_size = 40;
+
+function align() {
+    const body = document.getElementsByTagName('body')[0];
+    const grid = document.getElementsByClassName('grid')[0];
+
+    const full_width = +window.getComputedStyle(body).width.replace('px', '');
+    const grid_width = +window.getComputedStyle(grid).width.replace('px', '');
+
+    const theoretical_margin = (40 + full_width - grid_width) / 2;
+
+    const ratio = theoretical_margin / grid_size;
+    const full_columns = Math.floor(ratio);
+    const delta = ratio - full_columns > 0.5 ? 20 : -20;
+
+    let margin = full_columns * grid_size + delta;
+
+    if(margin + grid_width > full_width) {
+        margin = 20;
+    }
+
+    grid.style.marginLeft = Math.max(20, margin) + 'px';
+}
+
+function fix_height() {
+    [...document.getElementsByClassName('card')].forEach(function(el) {
+        const style = window.getComputedStyle(el);
+
+        const top = +style.marginTop.replace('px', '');
+        const height = +style.height.replace('px', '');
+
+        const full_height = top + height;
+
+        const missing_height = grid_size - (full_height % grid_size);
+
+        el.style.marginBottom = (missing_height > 39 ? 0 : missing_height) + 'px';
+    });
+}
+
+align();
+
+setTimeout(fix_height, 550);
+
+
+let timeout = false;
+window.addEventListener('resize', function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(align, 250);
+});
